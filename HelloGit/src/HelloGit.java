3c54e901e9b82015cf4f8875a112052e4f51e376
bit <bit@DESKTOP-881T9GE>
2018-03-22 15:17:18 +0900

    add

---
+++ HelloGit/src/HelloGit.java
@@ -0,0 +1,14 @@
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("1_hello_git");
+		//1. hotfix �߰� �� ������ �귻ġ ���� 
+		System.out.println("2_add_hotfix_git");
+		//2. marge �� ������ �귣ġ ���� !!
+		System.out.println("3_add_master");
+	}
+
+}
